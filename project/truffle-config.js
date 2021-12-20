dc641dd156b84f0ed946fd19ac47564c2f02e2c5
Felipe Sanchez <f.sanxz@gmail.com>
2021-12-19 21:15:20 -0300

    add rinkby network

--- project/truffle-config.js
+++ project/truffle-config.js
@@ -1,11 +1,26 @@
+const HDWallet = require('truffle-hdwallet-provider');
+const infuraKey = "fj4jll3k.....";
+
+const mnemonic = 'spirit supply whale amount human item harsh scare congress discover talent hamster'
+
 module.exports = {
   networks: {
     development: {
       host: "127.0.0.1",
       port: 8545,
       network_id: "*" // Match any network id
-    }
-  },  // Configure your compilers
+    },
+    // Useful for deploying to a public network.
+    // NB: It's important to wrap the provider as a function.
+    rinkeby: {
+      provider: () => new HDWalletProvider(process.env['RINKEBYNETMNEMONIC'], `https://rinkeby.infura.io/v3/${process.env['INFURATOKEN']}`);
+      network_id: 4,       // rinkeby's id
+      gas: 5500000,        // rinkeby has a lower block limit than mainnet
+      confirmations: 2,    // # of confs to wait between deployments. (default: 0)
+      timeoutBlocks: 200,  // # of blocks before a deployment times out  (minimum/default: 50)
+      skipDryRun: true     // Skip dry run before migrations? (default: false for public nets )
+    },
+  },
   compilers: {
     solc: {
       version: "0.8.0"
